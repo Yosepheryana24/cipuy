6dd8544e1d808753401e58b8756b49c187629a2f
Yosepheryana24 <Yosepozilheryana24@gmail.com>
2019-09-30 17:07:14 +0700

    menambahkan html dan js

---
+++ coba.js
@@ -0,0 +1,9 @@
+var angka = prompt('Masukkan angka yang anda inginkan');
+
+if (angka % 2 === 0) {
+    alert('Angka yang anda masukkan angka genap');
+} else if (angka % 2 === 1) {
+    alert('Angka yang anda masukkan angka ganjil');
+} else {
+    alert('yang anda masukkan bukan angka');
+}
